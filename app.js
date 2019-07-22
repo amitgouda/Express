122efd9494db7f69b30789bb5ca151d5f5c8c5e9
Amit <amitgouda17995@gmail.com>
2019-07-22 06:50:34 +0530

    22-07 1st commit

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-const http = require('http');
-
-//const routes = require('./routes');
+/* const http = require('http'); */
 
 const express = require('express');
 
 const app = express();
 
-//const server = http.createServer(app);
-
-app.use((req, res, next) => { 
-    console.log('In the middleware');
-    next();
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();  //Allows the request to conntinue to the next middle ware
 });
 
 app.use((req, res, next) => {
-    console.log('In the naothet middleware');
-    res.send("<h1>Hey its me</h1>");
+    console.log('In the middleware!');
+    res.send('<h1>Hello from express</h1>');
 });
 
-//console.log(routes.text)
-//server.listen(3000);
-app.listen(3000) 
+/* const server = http.createServer(app);
+
+server.listen(3000);   */ 
+app.listen(3000);
