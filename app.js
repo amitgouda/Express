5b884966be633748b5a5ce3121bd46fcf5eae7c4
Amit <amitgouda17995@gmail.com>
2019-07-22 07:27:22 +0530

    22 -07 commit 2

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 /* const http = require('http'); */
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
+ 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();  //Allows the request to conntinue to the next middle ware
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/',(req, res, next) => {
+    next();
 });
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/add-product',(req, res, next) => {
+    res.send("<form action='/product' method='POST' ><input type='text' name='title' /><button type ='submit'>Add Product</button></form>"); 
+});
+
+app.use('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
     res.send('<h1>Hello from express</h1>');
 });
 
